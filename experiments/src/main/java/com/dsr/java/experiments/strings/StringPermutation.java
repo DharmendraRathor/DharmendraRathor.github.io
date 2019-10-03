e00155046d35edd3a1eeddf3045fc14750af14f3
DharmendraRathor <31398624+DharmendraRathor@users.noreply.github.com>
2019-10-03 15:08:31 -0700

    Create StringPermutation.java

---
+++ experiments/src/main/java/com/dsr/java/experiments/strings/StringPermutation.java
@@ -0,0 +1,29 @@
+public class StringPermutation {
+
+	static void permutation(String str, String chosen) {
+		if (str.length() == 0) {
+			System.out.println(chosen);
+		} else {
+			char[] chars = str.toCharArray();
+			for(int i =0;i< chars.length;i++) {
+				// chose stage
+				char c = chars[i];      
+				chosen = chosen+c;
+				str = str.substring(str.indexOf(c)+1);
+				//System.out.println(" str " + str + " chosen " + chosen);
+				// explore stage
+				permutation(str,chosen);
+				
+				// unchoose stage
+				str = str + c;
+				chosen = chosen.substring(0, chosen.length()-1);
+			}
+
+		}
+	}
+
+	public static void main(String[] a) {
+		permutation("ABC", "");
+	}
+	
+}
